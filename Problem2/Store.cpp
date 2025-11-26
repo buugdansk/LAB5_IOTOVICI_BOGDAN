aedfc764ba1f2f5609aa88dc5b4a00b6e81d9927
buugdansk <bogai1002@gmail.com>
2025-11-26 22:10:26 +0200

    a

--- Problem2/Store.cpp
+++ Problem2/Store.cpp
@@ -6,14 +6,12 @@ Dept& Store::shoes() { return sDept; }
 
 Dept& Store::toys() { return tDept; }
 
-bool Store::search(string name) {
-    if (sDept.find(name)) return true;
-    if (tDept.find(name)) return true;
-    return false;
-}
-
 Product* Store::get(string name) {
     Product* p = sDept.get(name);
     if (p != nullptr) return p;
     return tDept.get(name);
+}
+
+bool Store::search(string name) {
+    return get(name) != nullptr;
 }
